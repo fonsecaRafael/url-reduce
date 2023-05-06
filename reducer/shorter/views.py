c6c250aa97c9212e82870d4966e7ee50e4aefe11
fonsecaRafael <rafael.abreu.fonseca@gmail.com>
2023-05-06 19:40:02 +0200

    Insert report view

--- reducer/shorter/views.py
+++ reducer/shorter/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import redirect, render
 from reducer.shorter.models import UrlRedirect
 
 
+def reports(request, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    shortened_url = request.build_absolute_uri(f'/{slug}')
+    context = {'reduce': url_redirect, 'shortened_url': shortened_url}
+    return render(request, 'shorter/report.html', context)
+
+
 def redirecter(request, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
     return redirect(url_redirect.destiny)
--- reducer/urls.py
+++ reducer/urls.py
@@ -7,4 +7,5 @@ from reducer.shorter import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<slug:slug>', views.redirecter),
+    path('reports/<slug:slug>', views.reports),
 ]
