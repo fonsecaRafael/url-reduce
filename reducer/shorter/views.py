b102b79b93871d17e93e29c698cdaa21ddaf0eb2
fonsecaRafael <rafael.abreu.fonseca@gmail.com>
2023-05-06 20:18:45 +0200

    Refactor redirecterview to get data

--- reducer/shorter/views.py
+++ reducer/shorter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from reducer.shorter.models import UrlRedirect
+from reducer.shorter.models import UrlLog, UrlRedirect
 
 
 def reports(request, slug):
@@ -12,4 +12,11 @@ def reports(request, slug):
 
 def redirecter(request, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
+    UrlLog.objects.create(
+        origin=request.META.get('HTTP_REFERER'),
+        user_agent=request.META.get('HTTP_USER_AGENT'),
+        host=request.META.get('HTTP_HOST'),
+        ip=request.META.get('REMOTE_ADDR'),
+        url_redirect=url_redirect
+    )
     return redirect(url_redirect.destiny)
