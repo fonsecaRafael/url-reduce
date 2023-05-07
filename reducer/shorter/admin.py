d0420d8cf882d4292819b006f7149a8843557ac5
fonsecaRafael <rafael.abreu.fonseca@gmail.com>
2023-05-07 06:44:00 +0200

    Insert UrlLogs in django admin

--- reducer/shorter/admin.py
+++ reducer/shorter/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from reducer.shorter.models import UrlRedirect
+from reducer.shorter.models import UrlLog, UrlRedirect
 
 
 @admin.register(UrlRedirect)
 class UrlRedirectAdmin(admin.ModelAdmin):
     list_display = ('destiny', 'slug', 'created_at', 'updated_at')
+
+
+@admin.register(UrlLog)
+class UrlLogAdmin(admin.ModelAdmin):
+    list_display = ('origin', 'created_at', 'user_agent',
+                    'host', 'ip', 'url_redirect')
+
+    def has_change_permission(self, request, obj=None) -> bool:
+        return False
+
+    def has_delete_permission(self, request, obj=None) -> bool:
+        return False
+
+    def has_add_permission(self, request, obj=None) -> bool:
+        return False
